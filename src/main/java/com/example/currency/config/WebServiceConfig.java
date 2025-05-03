0e20b160d55d9db4b723d1bc4d6a97c3b70476e0
wahib bachoua <wahibbachoua95@gmail.com>
2025-05-03 17:03:01 +0100

    exposition automatiquement du WSDL

---
+++ src/main/java/com/example/currency/config/WebServiceConfig.java
@@ -0,0 +1,41 @@
+package com.example.currency.config;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
+
+@EnableWs
+@Configuration
+public class WebServiceConfig extends WsConfigurerAdapter {
+    @Bean
+    public ServletRegistrationBean<MessageDispatcherServlet>
+    messageDispatcherServlet(ApplicationContext context) {
+        MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+        servlet.setApplicationContext(context);
+        servlet.setTransformWsdlLocations(true);
+        return new ServletRegistrationBean<>(servlet, "/ws/*");
+    }
+
+    @Bean(name = "currencyConverter")
+    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema currencySchema) {
+        DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
+        wsdl11Definition.setPortTypeName("CurrencyPort");
+        wsdl11Definition.setLocationUri("/ws");
+        wsdl11Definition.setTargetNamespace("http://example.com/currency");
+        wsdl11Definition.setSchema(currencySchema);
+        return wsdl11Definition;
+    }
+
+    @Bean
+    public XsdSchema currencySchema() {
+        return new SimpleXsdSchema(new ClassPathResource("currency-converter.xsd"));
+    }
+}
